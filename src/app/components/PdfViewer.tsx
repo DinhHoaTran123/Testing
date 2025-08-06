997a93d0846cc64a72fa825a849834ac3dad15f0
dinhhoatran <trandinhhoa1999@gmail.com>
2025-08-06 11:09:37 +0700

    Created for input field && table && tab

--- src/app/components/PdfViewer.tsx
+++ src/app/components/PdfViewer.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 
 export default function PdfViewer() {
